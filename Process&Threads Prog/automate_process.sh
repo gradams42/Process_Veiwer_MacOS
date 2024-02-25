75ae1b8a8f52151a0007c1326c5c274487b2be3c
gradams42 <gradams42@tntech.edu>
2024-02-24 23:09:36 -0600

    Update automate_process.sh

--- Process&Threads Prog/automate_process.sh
+++ Process&Threads Prog/automate_process.sh
@@ -1,11 +1,6 @@
 # Check if Homebrew is installed, install if not
 # /bin/bash is the location homebrew is supposed to be installed at, 
 # so the command goes into that directory for the installation
-if ! command -v brew &> /dev/null
-then
-    echo "Homebrew not found, installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
 
 # Check if python3 is installed, install if not
 if ! command -v python3 &> /dev/null
@@ -14,20 +9,28 @@ then
     brew install python3
 fi
 
+# Set up a virtual environment in the terminal
+# First Command
+python3 -m venv venv
+
+if ! command -v brew &> /dev/null
+then
+    echo "Homebrew not found, installing..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
+
+
 # Check if pip is installed, install if not
-if ! command -v pip &> /dev/null
+if ! command -v pip3 &> /dev/null
 then
     echo "Pip not found, installing..."
     # Install pip using easy_install
-    easy_install pip
+    easy_install pip3
 fi
 
 # Navigate to the "Process&Threads Prog" directory
 cd "Process& Threads Prog"
 
-# Set up a virtual environment in the terminal
-# First Command
-python3 -m venv venv
 
 # Second Command
 source venv/bin/activate
