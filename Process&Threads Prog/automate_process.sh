ced2acb6fe8e64c941427327978e49e404c0e3fb
gradams42 <gradams42@tntech.edu>
2024-02-26 09:10:48 -0600

    Created main.exe
    Utilized pyinstaller to create an executable file to run, without requiring any user to install dependencies.

--- Process&Threads Prog/automate_process.sh
+++ Process&Threads Prog/automate_process.sh
@@ -1,41 +1,51 @@
-#!/bin/bash
+# Check if Homebrew is installed, install if not
+# /bin/bash is the location homebrew is supposed to be installed at, 
+# so the command goes into that directory for the installation
 
-# Function to check if a command is available
-command_exists() {
-    command -v "$1" >/dev/null 2>&1
-}
 
-echo "Entering VIRTUAL ENVIRONMENT"
+
+# Check if curl is installed, install if not
+if ! command -v curl &> /dev/null
+then
+    echo "curl not found, please install it before running this script."
+    exit 1
+fi
+
+# Install Homebrew
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
+# Check if Homebrew installation was successful
+if command -v brew &> /dev/null
+then
+    echo "Homebrew installed successfully."
+else
+    echo "Homebrew installation failed. Please check the error messages above."
+fi
+
 
 # Check if python3 is installed, install if not
-if ! command_exists python3; then
+if ! command -v python3 &> /dev/null
+then
     echo "Python3 not found, installing..."
-    if command_exists brew; then
-        brew install python3
-    else
-        echo "Homebrew not found, please install Homebrew manually."
-        exit 1
-    fi
+    brew install python3
 fi
 
-# Check if Homebrew is installed, install if not
-if ! command_exists brew; then
-    echo "Homebrew not found, installing..."
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
-fi
-
-# Check if pip is installed, install if not
-if ! command_exists pip3; then
-    echo "Pip not found, installing..."
-    # Install pip using easy_install
-    easy_install pip3
-fi
+echo "Entering VIRTUAL ENVIORNMENT"
 
 # Set up a virtual environment in the terminal
 # First Command
 python3 -m venv venv
 
-# Activate the virtual environment
+# Check if pip is installed, install if not
+if ! command -v pip3 &> /dev/null
+then
+    echo "Pip not found, installing..."
+    # Install pip using easy_install
+    easy_install pip3
+fi
+
+
+# Second Command
 source venv/bin/activate
 
 # Install required dependencies located in requirements.txt "-r does it recursively"
